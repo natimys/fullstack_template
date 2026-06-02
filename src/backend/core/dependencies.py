3691884431400864f3570ef49def11334e1b9b2c
natimys <n4t1m.os@gmail.com>
2026-06-02 15:28:31 +0900

    fixed rbac dependency

--- src/backend/core/dependencies.py
+++ src/backend/core/dependencies.py
@@ -1,13 +1,20 @@
 from fastapi import Depends, HTTPException
 
+from database.dependencies import get_db
 from core.enums import UserRole
 from core.security import jwt_security
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from modules.users.dependencies import get_user_service
+from modules.users.service import UserService
 
 def require_role(*roles: UserRole):
-    def dependency(payload=Depends(jwt_security.access_token_required)):
-        if payload.role not in roles:
-            raise HTTPException(403)
-        return payload
-
+    async def dependency(
+        payload = Depends(jwt_security.access_token_required),
+        user_service: UserService = Depends(get_user_service),
+        db: AsyncSession = Depends(get_db)
+    ):
+        user = await user_service.get_user_by_id(int(payload.sub))
+        if user.role not in roles:
+            raise HTTPException(status_code=403)
+        return user
     return dependency
